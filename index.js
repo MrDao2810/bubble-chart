733090e253358270d7206c3561bb6fee532a4da3
create feature <dao.daoquyen833@gmail.com>
2022-06-22 01:22:31 +0700

    Create div element

--- index.js
+++ index.js
@@ -29,8 +29,42 @@
         return divElement;
     }
     
-    function createTickerItem() {
-        
-    }
+    function createTickerItem(tickerData, index) {
+        // Create ticker item div
+        let tickerItemDiv = createDiv('chart-ticker-item');
+        tickerItemDiv.setAttribute('name', tickerData.ticker);
+        tickerItemDiv.setAttribute('id', tickerData.ticker);
+        // Create ticker item name div
+        let tickerItemNameDiv = createDiv('chart-ticker-name', tickerData.ticker);
+        // Create ticker item capital img div
+        let tickerItemCapitalImgDiv = createDiv('chart-ticker-item-img');
+        tickerItemCapitalImgDiv.style.setProperty('width', tickerData.maxCapitalImgWidth + 'px');
+        tickerItemCapitalImgDiv.style.setProperty('height', tickerData.maxCapitalImgWidth + 'px');
+        tickerItemCapitalImgDiv.style.background = tickerData.color;
+        // Create ticker item capital text div
+        let tickerItemCapitalTextDiv = createDiv('chart-ticker-item-text', tickerData.capital);
+        // Construct ticker item div by append all children div
+        tickerItemDiv.append(tickerItemCapitalImgDiv);
+        tickerItemDiv.append(tickerItemNameDiv);
+        tickerItemDiv.append(tickerItemCapitalTextDiv);
+        return  tickerItemDiv;
 
+    }
+    function createTickerItemOfASpecificTime(tickerDataList) {
+        // 1. Clear the chart ticker list
+        chartTickerListDiv.innerHTML = '';
+        // 2. Sort the data list
+        tickerDataList.sort((item1, item2) => {
+            return item1.capital > item2.capital ? -1 : item1.capital < item2.capital? 1 : 0;
+        });
+        // 3. Calculate capital width
+        tickerDataList = calculateCapitalWidth(tickerDataList);
+        // 4. Create ticker item div and append to the chart-ticker-list div
+        for( let i = 0; i < tickerDataList.length; i++) {
+            let tickerData = tickerDataList[i];
+            const tickerItemDiv = createTickerItem(tickerData, i);
+            chartTickerListDiv.append(tickerItemDiv);
+        }
+    }
+    createTickerItemOfASpecificTime();
 })()
