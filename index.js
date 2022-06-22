d30676d19e18b7e868a20d983c70e3ffd17c8c07
CodeBro <daoquyen833@gmail.com>
2022-06-22 13:58:47 +0700

    Create bubble chart

--- index.js
+++ index.js
@@ -35,20 +35,31 @@
         tickerItemDiv.setAttribute('name', tickerData.ticker);
         tickerItemDiv.setAttribute('id', tickerData.ticker);
         // Create ticker item name div
-        let tickerItemNameDiv = createDiv('chart-ticker-name', tickerData.ticker);
+        let tickerItemNameDiv = createDiv('chart-ticker-item-name');
+        let tickerNameDiv = createDiv('chart-ticker-name', tickerData.ticker);
         // Create ticker item capital img div
         let tickerItemCapitalImgDiv = createDiv('chart-ticker-item-img');
-        tickerItemCapitalImgDiv.style.setProperty('width', tickerData.maxCapitalImgWidth + 'px');
-        tickerItemCapitalImgDiv.style.setProperty('height', tickerData.maxCapitalImgWidth + 'px');
+        tickerItemCapitalImgDiv.style.setProperty('width', tickerData.capitalImgWidth + 'px');
+        tickerItemCapitalImgDiv.style.setProperty('height', tickerData.capitalImgWidth + 'px');
         tickerItemCapitalImgDiv.style.background = tickerData.color;
         // Create ticker item capital text div
-        let tickerItemCapitalTextDiv = createDiv('chart-ticker-item-text', tickerData.capital);
+        let tickerItemCapitalTextDiv = createDiv('chart-ticker-item-capital-text');
+        let tickerCapitalTextDiv = createDiv('chart-ticker-item-text', tickerData.capital);
+        tickerItemCapitalTextDiv.append(tickerCapitalTextDiv);
         // Construct ticker item div by append all children div
+        tickerItemNameDiv.append(tickerNameDiv);
         tickerItemDiv.append(tickerItemCapitalImgDiv);
         tickerItemDiv.append(tickerItemNameDiv);
         tickerItemDiv.append(tickerItemCapitalTextDiv);
-        return  tickerItemDiv;
+        return tickerItemDiv;
 
+    }
+    function calculateCapitalWidth(sortedTickerDataList) {
+        const maxCapital = sortedTickerDataList[0].capital;
+        for(let tickerData of sortedTickerDataList) {
+            tickerData.capitalImgWidth = tickerData.capital;
+        }
+        return sortedTickerDataList;
     }
     function createTickerItemOfASpecificTime(tickerDataList) {
         // 1. Clear the chart ticker list
@@ -66,5 +77,14 @@
             chartTickerListDiv.append(tickerItemDiv);
         }
     }
-    createTickerItemOfASpecificTime();
+    function playOneYear() {
+        playAYear(selectedYear);
+    }
+    function playAYear(year) {
+        let specificYearTickerData = vn30Data[year];
+        if(specificYearTickerData) {
+            createTickerItemOfASpecificTime(specificYearTickerData);
+        }
+    }
+    createTickerItemOfASpecificTime(vn30Data['2000']);
 })()
