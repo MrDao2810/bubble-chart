7b9f39be4793d8cfa1d2a4aef1c9fb5548381c48
CodeBro <daoquyen833@gmail.com>
2022-06-27 16:28:02 +0700

    Add chartTicheristCanvasText

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 (function() {
     const chartTickerListDiv = document.getElementsByClassName('chart-ticker-list')[0];
+    const chartTickerListCanvas = document.getElementsByClassName('chart-ticker-list-canvas')[0];
+    let chartTickerListCanvasText = chartTickerListCanvas.getContext("2d");
     const runOneYearSelectorText = document.getElementsByClassName('run-one-year-selector')[0];
     const runOneYearButton = document.getElementsByClassName('run-one-year-button')[0];
     let selectedYear = runOneYearSelectorText.value;
@@ -36,25 +38,22 @@
     
     function createTickerItem(tickerData, index) {
         // Create ticker item div
-        let tickerItemDiv = createDiv('chart-ticker-item');
+        let tickerItemDiv = createDiv('chart-ticker-item'); 
         tickerItemDiv.setAttribute('name', tickerData.ticker);
         tickerItemDiv.setAttribute('id', tickerData.ticker);
-        tickerItemDiv.style.setProperty('width', tickerData.capitalItem + 'px');
-        tickerItemDiv.style.setProperty('height', tickerData.capitalItem + 'px');
-        console.log(index);
+        // tickerItemDiv.style.width = tickerData.capitalItem + 'px';
+        // tickerItemDiv.style.height = tickerData.capitalItem + 'px'; 
         tickerItemDiv.style.background = tickerData.color;
         // Create ticker item name div
         let tickerItemNameDiv = createDiv('chart-ticker-item-name', tickerData.ticker);
         // Create ticker item capital text div
-        let tickerItemCapitalTextDiv = createDiv('chart-ticker-item-capital-text', tickerData.capital);
+        let tickerItemCapitalTextDiv = createDiv('chart-ticker-item-capital-text', tickerData.capitalItem);
         // Construct ticker item div by append all children div
         tickerItemDiv.append(tickerItemNameDiv);
         tickerItemDiv.append(tickerItemCapitalTextDiv);
-        return tickerItemDiv;
+        return tickerItemDiv;     
     }
-    // function createWidthTickerDataDiv() {
-    // }
-    
+
     function calculateCapitalWidth(sortedTickerDataList) {
         // const maxCapital = createWidthTickerDataDiv(sortedTickerDataList.capital);
         for(let tickerData of sortedTickerDataList) {
@@ -73,12 +72,38 @@
         // 3. Calculate capital width
         tickerDataList = calculateCapitalWidth(tickerDataList);
         // 4. Create ticker item div and append to the chart-ticker-list div
-        for( let i = 0; i < tickerDataList.length; i++) {
-            let tickerData = tickerDataList[i];
-            const tickerItemDiv = createTickerItem(tickerData, i);
-            chartTickerListDiv.append(tickerItemDiv);
+        let linesContainingCircle = 5;
+        for ( let i = 0; i < linesContainingCircle; i++) {
+            for( let j = 0; j < tickerDataList.length; j++) {
+                let tickerData = tickerDataList[j];
+                let areaOfPreviousSquare;
+                if (j = 0) {
+                    areaOfPreviousSquare = tickerData.capitalItem * tickerData.capitalItem;
+                } else {
+                    let tickerDataPrev = tickerDataList[j - 1];
+                    console.log(tickerDataPrev);
+                    areaOfPreviousSquare = tickerDataPrev.capitalItem * tickerDataPrev.capitalItem;
+                }
+                console.log(areaOfPreviousSquare);
+                const tickerItemDiv = createTickerItem(tickerData, j);
+                chartTickerListCanvasText.beginPath();
+                // Trục hoành x của đồ thị
+                let graphHorizontalAxis = areaOfPreviousSquare + (tickerData.capitalItem/2);
+                // Trục tung y của đồ thị 
+                let graphVerticalAxis = areaOfPreviousSquare + tickerData.capitalItem/2;
+                // Bán kính đường tròn
+                let circleRadius = tickerData.capitalItem;
+                // Vị trí của đường tròn 
+                chartTickerListCanvasText.arc(graphHorizontalAxis, graphVerticalAxis, circleRadius, 0, Math.PI * 2, true);
+                // stroke Để hiển thị các nét vẽ mà mình đã định.
+                chartTickerListCanvasText.stroke();
+                chartTickerListCanvas.append(tickerItemDiv);
+                chartTickerListDiv.append(chartTickerListCanvas);
+            }
+            return;
         }
     }
+
     function playOneYear() {
         let selectedYear = runOneYearSelectorText.value;
         yearCurrent.innerHTML = 'Year current: ' + ( +selectedYear );
